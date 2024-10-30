7970a8a6b92f3e20ee6c7f0ed6cf22a20ad26ffa
starwalker00 <aaronwalkerup@gmail.com>
2024-10-30 15:49:32 +0100

    more in cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,10 @@
-python main.py config.toml --database.port=1111 --database.username=new_user --aaa.param1=new_value
+#!/bin/bash
+
+# Commande pour exécuter le script Python avec le fichier de configuration
+python main.py config.toml
+
+# Exécution avec des paramètres supplémentaires
+python main.py config.toml --database.port="1111" --database.username=new_user
+
+# Ajoutez d'autres commandes d'exécution selon vos besoins
+python main.py config.toml --aaa.param1=valeur1
